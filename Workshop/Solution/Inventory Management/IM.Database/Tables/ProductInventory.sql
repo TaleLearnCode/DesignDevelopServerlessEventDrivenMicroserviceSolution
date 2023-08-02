bbc62567717d943686d5ae468c8b15476d265209
Chad Green <chadgreen@chadgreen.com>
2023-08-02 00:19:58 -0400

    Adding the Inventory Management database

---
+++ Workshop/Solution/Inventory Management/IM.Database/Tables/ProductInventory.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Inventory.ProductInventory
+(
+  ProductInventoryId       INT          NOT NULL IDENTITY(1,1),
+  ProductId                CHAR(5)      NOT NULL,
+  ProductInventoryActionId INT          NOT NULL,
+  ActionDateTime           DATETIME2    NOT NULL CONSTRAINT dfProductInventory_ActionDateTime DEFAULT(GETUTCDATE()),
+  InventoryCredit          INT          NOT NULL CONSTRAINT dfProductInventory_InventoryCredit DEFAULT(0),
+  InventoryDebit           INT          NOT NULL CONSTRAINT dfProductInventory_InventoryDebit DEFAULT(0),
+  OrderNumber              VARCHAR(100)     NULL,
+  CONSTRAINT pkcProductInventory PRIMARY KEY CLUSTERED (ProductInventoryId),
+  CONSTRAINT fkProductInventory_Product FOREIGN KEY (ProductId) REFERENCES Inventory.Product (ProductId),
+  CONSTRAINT fkProductInventory_ProductInventoryAction FOREIGN KEY (ProductInventoryActionId) REFERENCES Inventory.ProductInventoryAction (ProductInventoryActionId)
+)
---
+++ Workshop/Solution/Inventory Management/IM.Database/Tables/ProductInventoryAction.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Inventory.ProductInventoryAction
+(
+  ProductInventoryActionId   INT NOT NULL,
+  ProductInventoryActionName NVARCHAR(100),
+  CONSTRAINT pkcProductInventoryAction PRIMARY KEY CLUSTERED (ProductInventoryActionId)
+)
